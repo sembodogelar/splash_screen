eff942ed0bff296de9c39bc796aaac073f8267e4
SembodoGelar <sembodogelar6@gamail.com>
2024-06-19 00:59:22 +0700

    correct app.js

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo_header');
-let logoSpan = document.querySelector('.logo')
+let logo = document.querySelector('.logo-header');
+let logoSpans = document.querySelectorAll('.logo');
 
-window.addEventListener('DOMContentLoaded', ()=>{
-
-    setTimeout(()=>{
-
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        logoSpans.forEach((span, idx) => {
+            setTimeout(() => {
                 span.classList.add('active');
-            }, (idx + 1) * 400) 
-        })
-
-
-
-    })
-})
-    
+            }, (idx + 1) * 400);
+        });
+    }, 0);
+});
