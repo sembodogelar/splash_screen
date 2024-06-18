cf383ee6ce29645bc7cef20cef806ed6807be322
SembodoGelar <sembodogelar6@gamail.com>
2024-06-19 01:28:29 +0700

    correct and update app.js

--- app.js
+++ app.js
@@ -9,5 +9,18 @@ window.addEventListener('DOMContentLoaded', () => {
                 span.classList.add('active');
             }, (idx + 1) * 400);
         });
+
+        setTimeout(() => {
+            logoSpans.forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50);
+            });
+        }, 2000);
+
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        }, 2300);
     }, 0);
 });
